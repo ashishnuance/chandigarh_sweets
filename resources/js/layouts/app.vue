da5dc257b9e87fc18d0e34c2cba0ff85b10df846
dev <dev@gmail.com>
2023-09-28 20:21:24 +0530

    routing

--- resources/js/layouts/app.vue
+++ resources/js/layouts/app.vue
@@ -5,5 +5,7 @@
             <router-link to="/front/login">login</router-link>
             <router-link to="/front/register">register</router-link>
         </div>
+
+        <router-view></router-view>
     </div>
 </template>
