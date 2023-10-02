98e3d300c98aa305d2b9246d4618f1844faf398b
dev <dev@gmail.com>
2023-10-02 14:31:05 +0530

    company-admin and company-user issues resolved

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -13,16 +13,33 @@ class UsersExport implements FromCollection,WithHeadings
     */
     public function collection()
     {
-        return User::select("users.name","email","phone","address","country","state","city","website_url","image","blocked","created_at","updated_at")->whereHas(
-            'role', function($q){
-                $q->where('name', 'company-user');
-            }
-        )->get();
+        $userRecord = User::with('company')->select("users.name","email","phone","address","country","state","city","website_url","image","blocked","created_at","updated_at")->whereHas(
+        'role', function($q){
+            $q->where('name', 'company-user');
+        }
+        )->get()->map(function($userVal){
+            return [
+                'id'=>$userVal->id,
+                'company_name'=>(isset($userVal->company[0]->company_name) && $userVal->company[0]->company_name!='') ? $userVal->company[0]->company_name : '',
+                'name'=>$userVal->name,
+                'email'=>$userVal->email,
+                'phone'=>$userVal->phone,
+                'address'=>$userVal->address,
+                'country'=>(isset($userVal->countryname->name) && $userVal->countryname->name!='') ? $userVal->countryname->name : '',
+                'state'=>(isset($userVal->statename->name) && $userVal->statename->name!='') ? $userVal->statename->name : '',
+                'city'=>(isset($userVal->cityname->name) && $userVal->cityname->name!='') ? $userVal->cityname->name : '',
+                'website_url'=>$userVal->website_url,
+                'image'=>($userVal->image!=null) ? route('image.displayImage',[$userVal->image]): '',
+                'blocked'=>($userVal->blocked==1) ? 'UnBlocked' : 'Blocked',
+                'created_at'=>$userVal->created_at,
+            ];
+        });
+        return $userRecord;
     }
 
     public function headings(): array
     {
-        return ["Name","Email","Phone","Address","Country","State","City","Website Url","Image","Blocked","Created date","Updated date"
-    ];
+        return ["id","company name","name","email","phone","address","country","state","city","website_url","image","blocked","created"
+        ];
     }
 }
